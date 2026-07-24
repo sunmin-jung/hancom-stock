6e37b8ed450e98ace50702c4a9ff62b392f64c2e
sunmin-jung <wjdtjsals91@gmail.com>
2026-07-24 16:34:50 +0900

    add hooks

---
+++ plugins/usd-krw-plugin/scripts/log-search.ps1
@@ -0,0 +1,4 @@
+﻿$timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+$logLine = "$timestamp 환율 관련 웹검색이 실행되었습니다"
+Add-Content -Path "$env:TEMP\usd-krw-search-log.txt" -Value $logLine -Encoding UTF8
+Write-Host "🔍 환율 검색 로그 기록됨"
